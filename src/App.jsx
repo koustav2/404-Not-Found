e385bb39eb66b3be9592699b6eeeb057e3a0f29e
koustav <koustavm.aeie2018@nsec.ac.in>
2023-04-19 21:27:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+      <div className='mt-5 ml-10'>
+        <h1>404 Not Found </h1>
+
+      </div>
+      <div className='grid  m-auto container justify-center  '>
+        <div className='sm:flex flex-row gap-10 justify-center items-center'>
+          <div>
+            <img src="/assets/Scarecrow.png" alt="" className='w-' />
+          </div>
+          <div className='grid grid-rows-1'>
+            <h2 className='box-border'>
+              I had bad
+              news for you
+            </h2>
+            <div className='border-none border-sky-500 mt-7 mb-10 content'>
+              <p>The page you are looking for might be removed or is temporarily unavailable</p>
+            </div>
+            <button >Back to homepage</button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default App
